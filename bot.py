fdc85d30cb59f205e4a184de2a9059d8948c4e94
andrey <lastxaust@gmail.com>
2024-09-14 00:02:58 +0300

    add database.py and google_table

--- bot.py
+++ bot.py
@@ -2,17 +2,32 @@ import logging
 import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from google_table import GooogleWorksheets
+from database import DataBase
+import message_texts
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
+
+logger = logging.getLogger(__name__)
+
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_TOKEN')
+if not TELEGRAM_BOT_TOKEN:
+    exit("Specify TELEGRAM_TOKEN env variable")
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    effective_chat = update.effective_chat
+    if not effective_chat:
+        logger.warning("effective_chat is None")
+        return
+    await context.bot.send_message(chat_id=effective_chat.id, 
+                                   text=message_texts.GREETINGS)
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(os.getenv('TELEGRAM_TOKEN')).build()
+    application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
     
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
